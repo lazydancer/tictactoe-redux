013ebaba72303486ca2c68e82df3fb5d1a477e32
lazydancer <jrpucula@gmail.com>
2018-10-11 16:01:11 -0400

    Clean up App, logic now in containers

--- src/components/App.js
+++ src/components/App.js
@@ -3,125 +3,13 @@ import React from 'react'
 import BoardContainer from '../containers/BoardContainer'
 import GameInfoContainer from '../containers/GameInfoContainer'
 
-import Board from '../components/Board'
-import GameInfo from '../components/GameInfo'
-
 import './App.css'
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    })
-
-
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    })
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.reduce((acc, step, move) => {
-
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';      
-
-      let result = acc.concat([{
-        move: move,
-        desc: desc
-      }])
-
-      return result
-    }, [])
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div className="game">
-        <BoardContainer />
-        <GameInfoContainer />
-      </div>
-    );
-  }
-}
-
-/*
-
-    return (
-      <div className="game">
-        <Board
-          squares={current.squares}
-          onClick={(i) => this.handleClick(i)}
-        />
-        <GameInfo
-          status={status}
-          moves={moves}
-          history={history}
-        />
-        
-      </div>
-    );
-
-*/
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 const App = () => (
-    <Game />
+  <div className="game">
+    <BoardContainer />
+    <GameInfoContainer />
+  </div>
 )
 
 export default App
